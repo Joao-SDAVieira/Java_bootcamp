2c059f5efbd98f5cc9e11eb0c749649619b76ba0
Joao_SDAVieira <eujoaovictor0707@gmail.com>
2025-07-29 22:26:32 -0300

    First commit

---
+++ Hellow/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        final int CURRENT_YEAR = 2025;
+        String name = "João";
+        int birthYear = 2004;
+        int age = CURRENT_YEAR - birthYear;
+        System.out.println(age);
+
+    }
+}
